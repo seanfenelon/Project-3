35650ab71df6f3cfedddd691517abed12c30c8d1
danielzt94 <danieltockan@gmail.com>
2020-11-12 12:59:28 +0000

    roouter and contorllers

---
+++ Backend/Controllers/Resorts.js
@@ -0,0 +1,48 @@
+const Resorts = require('../Models/Resorts')
+const axios = require('axios')
+
+function singleProxyResort(req, res) {
+}
+
+function getResort(req, res) {  
+}
+
+function addResort(req, res) {
+
+}
+
+function singleResort(req, res) {
+
+}
+
+function removeResort(req, res) {
+
+}
+
+function editResort(req, res) {
+
+}
+
+function createComment(req, res) {
+
+}
+
+function editComment(req, res) {
+
+}
+
+function deleteComment(req, res) {
+
+}
+
+module.exports = {
+  singleProxyResort,
+  getResort,
+  addResort,
+  singleResort,
+  removeResort,
+  editResort,
+  createComment,
+  editComment,
+  deleteComment
+}
