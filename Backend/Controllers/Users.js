430034df1f43e84b8066464a00f080ac23337b61
Sean Fenelon <sean.fenelon93@gmail.com>
2020-11-12 16:14:24 +0000

    server-sean

--- Backend/Controllers/Users.js
+++ Backend/Controllers/Users.js
@@ -3,10 +3,23 @@ const jwt = require('jsonwebtoken')
 const { secret } = require('../Config/Environment')
 
 function createUser(req, res) {
+  const body= req.body
+  console.log(body)
+  User
+    .create(body)
+    .then(user => {
+      console.log(user)
+      res.send(user)
+    })
+    .catch(error => res.send(error))
 }
 
 function logInUser(req, res) {
-
+  User
+    .findOne({ email: req.body.email })
+    .then(user => {
+      
+    })
 }
 
 module.exports = {
---
+++ Backend/Server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const Router = require('./Router')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const expressServer = express()
+
+mongoose.connect(
+  'mongodb://localhost/resortdb',
+  { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
+  (err) => {
+    if (err) console.log(err)
+    else console.log('Mongoose connected successfully!')
+  }
+)
+
+expressServer.use((req, res, next) => {
+  console.log(`Incoming request, ${req.method} to ${req.url}`)
+  next()
+})
+
+expressServer.use(bodyParser.json())
+
+express.use('/api', Router)
+
+expressServer.listen(8000)
