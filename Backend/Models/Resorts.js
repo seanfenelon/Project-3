b52c6b7d1af93a78366ff649929ed6c8ae157268
Sean Fenelon <sean.fenelon93@gmail.com>
2020-11-12 12:54:13 +0000

    1pm-12-11

---
+++ Backend/Models/Resorts.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const resortSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  country: { type: String, required: true },
+  top_elevation: { type: Number, required: true },
+  bottom_elevation: { type: Number, required: true },
+  lon: { type: Number, required: true },
+  lat: { type: Number, require: true }
+
+})
+
+module.exports = mongoose.model('Resorts', resortSchema)
